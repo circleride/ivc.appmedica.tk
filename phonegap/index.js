10e1637eaace61b2d0fa64aa6ee0d198c98b7187
maitret <maitret@myhostmx.com>
2016-08-12 08:15:20 -0500

    :)

--- phonegap/index.js
+++ phonegap/index.js
@@ -147,7 +147,7 @@ carga_app: function(){
 window.url_target = function url_target(page,id){
 if(id == "undefined" || id == null || id == ""){ id = ""; }
 if(page == "undefined" || page == null || page == ""){ page = ""; }
-alert(page+" : "+id);
+//alert(page+" : "+id);
 $('.ajax-content').html('<div align="center"><br><br><h4><i class="fa fa-spinner fa-spin"></i></h4><br></div>');
 $.getJSON(window.url_server+"/movil/html.templates.php", { key: window.my_uuid, html: page, id: id, view_as: 'json' }, function (j) {
 var stateObj = { html: page };
