c231fac6045b3ef3c5cb4efb3889d577012a2ba4
maitret <maitret@myhostmx.com>
2016-08-12 06:58:45 -0500

    :)

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ console._log_old = console.log; console.log = function(msg){ alert("log: "+msg);
 console._error_old = console.error; console.error = function(msg){ alert("error: "+msg); console._error_old(msg); };
 console._warn_old = console.warn; console.warn = function(msg){ alert("warn: "+msg); console._warn_old(msg); };
 
+window.GetPushNotif = function GetPushNotif(data) {
+var data2 = data.additionalData;
+if(data2.action == "load_html"){
+alert("load_html: "+data2.data_action);
+}
+};
+
 var app = {
 initialize: function() {
 this.bindEvents();
@@ -31,29 +38,16 @@ push.on('registration', function(data) {
 $(".push_notif_log").load("https://ivc.appmedica.tk/tools/push_api.php?action=registration&push="+JSON.stringify(data)+"&device="+JSON.stringify(device));
 });
 push.on('notification', function(data) {
-var data2 = data.additionalData;
 alert(JSON.stringify(data));
 $(".push_notif_log").load("https://ivc.appmedica.tk/tools/push_api.php?action=notification&push="+JSON.stringify(data)+"&device="+JSON.stringify(device));
 /* data.message, data.title, data.count, data.sound, data.image, data.additionalData */
 if(typeof GetPushNotif == 'function') { window.GetPushNotif(data); } else { window.GetPushNotif = data; }
-//window.setBadge(data2.badge);
 });
 push.on('error', function(e) {
 $(".push_notif_log").load("https://ivc.appmedica.tk/tools/push_api.php?action=error&push="+JSON.stringify(e)+"&device="+JSON.stringify(device));
 // e.message
 });
 
-window.setBadge = function setBadge(num) {
-if(num != ""){ push.setApplicationIconBadgeNumber(function() { }, function() { }, num); }
-};
-
 //console.log('Received Event: ' + id);
-}
-};
-
-window.GetPushNotif = function GetPushNotif(data) {
-data2 = data.additionalData;
-if(data2.action == "load_html"){
-alert("load_html: "+data2.data_action);
 }
 };
